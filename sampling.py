bab61bb9c10e7828feee9ceb293172c3d96dbd73
Roger Wechsler <mr.mutator@gmail.com>
2015-03-21 23:28:31 +0100

    fixes.

--- sampling.py
+++ sampling.py
@@ -101,7 +101,7 @@ def get_dataset_likelihood(raw_dataset, root_counts, tree_counts):
 
     infile.close()
 
-    new_init = re.sub("grammar=.*?", "grammar=tmp_grammar.cfg")
+    new_init = re.sub("grammar=.*", "grammar=tmp_grammar.cfg", init)
 
     outfile = open(TMP_DATA_DIR + "tmp_init.ini", "w")
     outfile.write(new_init)
@@ -335,13 +335,13 @@ def metropolis_hastings(raw_dataset, old_dataset, n=1000, ap=None, outfile=sys.s
     global treeFrequency
     global rootFrequency
     
-    old_likelihood = get_dataset_likelihood(old_dataset)
+    #old_likelihood = get_dataset_likelihood(old_dataset)
 
-    outfile.write("\t".join(["0", "A", str(old_likelihood), str(old_likelihood), str(len(treeFrequency.keys())), str(np.sum(treeFrequency.values()))]) + "\n")
+    #outfile.write("\t".join(["0", "A", str(old_likelihood), str(old_likelihood), str(len(treeFrequency.keys())), str(np.sum(treeFrequency.values()))]) + "\n")
 
     for i in range(n):
 
-        new_dataset, new_tsg, _, _ = make_random_candidate_change(old_dataset)
+        new_dataset = make_random_candidate_change(old_dataset)
         new_likelihood = get_dataset_likelihood(raw_dataset, rootFrequency, treeFrequency) # lqrz: by passing the old and new block we can forloop only once to ge the likelihood.
         #if new_dataset == old_dataset:
         #    print "EQUAL!!"
@@ -487,7 +487,7 @@ parser = Parser(INITIAL_INI, CDEC_PATH)
 
 parses = []
 raw_dataset = []
-for s in data:
+for s in data.keys()[:10]:
     raw_dataset.append(s)
     s = ' '.join(str(s))
     parses.append(parser.get_best_parse(s))
